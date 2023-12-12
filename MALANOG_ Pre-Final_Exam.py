13bcc7de18ff1ffc1fdd7c24cd7e4a7f665c25f0
adtm0 <145269995+adtm0@users.noreply.github.com>
2023-12-12 23:47:53 +0800

    Add files via upload

---
+++ MALANOG_ Pre-Final_Exam.py
@@ -0,0 +1,29 @@
+n_start = eval(input("Enter a number [start]: "))
+
+if n_start == 0:
+    print("Program terminated.")
+    exit()
+
+while (n_start < 0):
+     print("Enter a non-negative number.")
+     n_start = eval(input("\nEnter a number [start]: "))
+     
+n_end = eval(input("Enter a number [end]: "))
+
+while (n_end < n_start):
+     print(f"Enter a number greater than {n_start}.")
+     n_end = eval(input("\nEnter a number [end]: "))
+
+prime_list = []
+for i in range(n_start, n_end + 1):
+    
+    if i == 1:
+        continue
+    
+    for j in range(2,i):
+        if (i % j) == 0:
+            break
+    else:
+            prime_list.append(i)
+            
+print(f"Prime numbers between 1 and 10 are: {str(prime_list).strip('[],').replace(',','')}")
